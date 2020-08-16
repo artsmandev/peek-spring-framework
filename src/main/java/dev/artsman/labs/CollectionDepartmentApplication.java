7aeb5825a24ee808d9a639e3dee929f8f92ccd41
Bruno Andrade <bruno@artsman.dev>
2020-08-16 10:54:45 -0300

    base project.

---
+++ src/main/java/dev/artsman/labs/CollectionDepartmentApplication.java
@@ -0,0 +1,13 @@
+package dev.artsman.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionDepartmentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CollectionDepartmentApplication.class, args);
+  }
+
+}
